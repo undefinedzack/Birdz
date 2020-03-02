3bdb63d333093cd84d0f8d866ab16627e24de47b
undefinedzack <adhney.nawghare@gmail.com>
2020-03-02 23:23:03 +0530

    its done

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        Birdie bird = collision.collider.GetComponent<Birdie>();
+        
+        if(bird != null){
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if(enemy != null){
+            return;
+        }
+
+        if(collision.contacts[0].normal.y < - 0.5){
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
